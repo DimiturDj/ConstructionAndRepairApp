e8170649ef1a1c987c104f962f69f35f9301d428
DimiturDj <167714606+DimiturDj@users.noreply.github.com>
2025-02-11 21:58:21 +0200

    Models

---
+++ Review.cs
@@ -0,0 +1,15 @@
+﻿namespace RepairAndConstructionService.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public Customer Customer { get; set; }
+
+        public int WorkerId { get; set; }
+        public Worker Worker { get; set; }
+
+        public string Comment { get; set; }
+        public int Rating { get; set; } // 1 to 5 stars
+    }
+}
